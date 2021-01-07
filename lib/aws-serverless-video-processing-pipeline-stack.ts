654201825c80cec8c62b61e040dbbdac4e4c7b37
Philip Chen <philiche@amazon.com>
2021-01-06 16:59:29 -0800

    Add PreProd Stage

--- lib/aws-serverless-video-processing-pipeline-stack.ts
+++ lib/aws-serverless-video-processing-pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { AwsServerlessVideoProcessingStage } from './aws-serverless-video-procesing-stage';
 
 /**
  * The stack that defines the application pipeline
@@ -39,6 +40,6 @@ export class AwsServerlessVideoProcessingPipelineStack extends Stack {
         });
 
         // This is where we add the application stages
-        // ...
+        pipeline.addApplicationStage(new AwsServerlessVideoProcessingStage(this, 'PreProd'));
     }
 }
