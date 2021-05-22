c525ed6024edc3e23a4cd246b9b2eca42aebd1bd
Philip Chen <philip.chen4@gmail.com>
2021-05-22 13:34:02 -0700

    Add missed files to repository

---
+++ video-processing-cdk/lambda/index.py
@@ -0,0 +1,38 @@
+import boto3
+import urllib.parse
+import shlex
+import subprocess
+import os
+
+# Environment Variables
+S3_DESTINATION_BUCKET=os.environ.get('S3_DESTINATION_BUCKET')
+
+# Constants
+S3_SIGNED_URL_EXPIRATION=120
+FFMPEG_PARAMS='-f mpegts -c:v copy -af aresample=async=1:first_pts=0 -'
+
+# Clients
+s3 = boto3.client('s3')
+
+def handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    s3_signed_url_get_object = s3.generate_presigned_url('get_object',
+        Params={'Bucket': bucket, 'Key': key},
+        ExpiresIn=S3_SIGNED_URL_EXPIRATION)
+    
+    ffmpeg_cmd = '/opt/bin/ffmpeg -i \"' + s3_signed_url_get_object + '\"' + FFMPEG_PARAMS
+    cmd = shlex.split(ffmpeg_cmd)
+    process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output_key = os.path.splitext(key)[0] + ".ts"
+    response = s3.put_object(Body=process.stdout, Bucket=S3_DESTINATION_BUCKET, Key=output_key)
+
+    return {
+        'statusCode': 200,
+        'inputBucket': bucket,
+        'inputKey': key,
+        'outputBucket': S3_DESTINATION_BUCKET,
+        'outputKey': output_key
+    }
--- video-processing-cdk/lib/video-processing-cdk-stack.ts
+++ video-processing-cdk/lib/video-processing-cdk-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3n from '@aws-cdk/aws-s3-notifications';
 
 export class VideoProcessingCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const inputBucket = new s3.Bucket(this, 'InputVideoBucket');
+    const outputBucket = new s3.Bucket(this, 'OutputVideoBucket');
+
+    const layer = new lambda.LayerVersion(this, 'FfmpegLayer', {
+      description: 'Lambda layer for ffmpeg',
+      code: lambda.Code.fromAsset('lambda/layer/ffmpeg-4.4.zip'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_8],
+      license: 'GPLv3',
+    });
+
+    const fn = new lambda.Function(this, 'VideoProcessingFn', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.PYTHON_3_8,
+      layers: [layer],
+      environment: {
+        'S3_DESTINATION_BUCKET': outputBucket.bucketName
+      }
+    });
+    inputBucket.addEventNotification(s3.EventType.OBJECT_CREATED_PUT, new s3n.LambdaDestination(fn));
+    inputBucket.grantRead(fn);
+    outputBucket.grantWrite(fn);
   }
 }
